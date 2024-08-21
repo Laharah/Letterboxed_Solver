697a4af186b44a0c5480f28aa2565b784d636b81
laharah <laharah22@gmail.com>
2024-08-21 15:06:06 -0700

    feat(non_nan): Reverse `Ord` and `PartialOrd` to conform to convention.

--- src/non_nan.rs
+++ src/non_nan.rs
@@ -3,16 +3,16 @@ use std::cmp::Ordering;
 #[derive(Debug, PartialEq)]
 pub struct OrderedF32(pub f32);
 
-impl PartialOrd for OrderedF32 {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.0.partial_cmp(&other.0)
+impl Ord for OrderedF32 {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.partial_cmp(&other.0).unwrap()
     }
 }
 
 impl Eq for OrderedF32 {}
 
-impl Ord for OrderedF32 {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
+impl PartialOrd for OrderedF32 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
